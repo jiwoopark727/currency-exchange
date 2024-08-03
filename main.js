a723bff821605f0fc8c729aa3543d3d3222f7bc8
Woo <jiwoopark727@gmail.com>
2024-08-03 18:23:59 +0900

    드롭다운 리스트에서 선택한 화폐로 버튼 텍스트 교체

--- main.js
+++ main.js
@@ -10,16 +10,38 @@ let currencyRatio = {
   USD:{
     USD:1,
     KRW:1373.86,
-    VND:25219.50
+    VND:25219.50,
+    unit:"달러"
   },
   KRW:{
     KRW:1,
     USD:0.00073,
-    VND:18.36
+    VND:18.36,
+    unit:"원"
   },
   VND:{
     VND:1,
     USD:0.00004,
-    KRW:0.054
+    KRW:0.054,
+    unit:"동"
   }
 }
+
+let fromCurrency = 'USD'
+let toCurrency = 'USD'
+
+document.querySelectorAll('#from-currency-list a').forEach(menu=>menu.addEventListener("click",function(){
+  //버튼 가져와서 클릭한 메뉴의 값을 텍스트를 교체
+  document.getElementById("from-button").textContent = this.textContent;
+
+  //클릭한 메뉴의 값을 변수에 저장해두기
+  fromCurrency = this.textContent;
+}))
+
+document.querySelectorAll('#to-currency-list a').forEach(menu=>menu.addEventListener("click",function(){
+  //버튼 가져와서 클릭한 메뉴의 값을 텍스트를 교체
+  document.getElementById("to-button").textContent = this.textContent;
+
+  //클릭한 메뉴의 값을 변수에 저장해두기
+  fromCurrency = this.textContent;
+}))
